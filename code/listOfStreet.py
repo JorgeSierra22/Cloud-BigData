4bba5e63c3b50d9ced7dc9c34d5c7b7b81b77c99
PabloCG13 <112969906+PabloCG13@users.noreply.github.com>
2023-12-07 14:11:32 +0100

    nuevo código
    Calles atendidas por comisaria y su csv

---
+++ code/listOfStreet.py
@@ -0,0 +1,33 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, split, concat_ws, collect_set, upper
+
+# Configuración de Spark
+conf = SparkConf().setAppName('StreetListByDistrict')
+sc = SparkContext(conf=conf)
+sc.setLogLevel('ERROR')
+spark = SparkSession(sc)
+
+# Reemplaza 'Crimes_-_2001_to_Present.csv' con la ruta real de tu archivo de datos de crímenes
+crime_df = spark.read.option("header", "true").csv("Crimes_-_2001_to_Present.csv")
+
+# Divide el campo 'Block' por espacios y selecciona los elementos 2 y 3
+crime_df = crime_df.withColumn("BlockArray", split(col("Block"), " "))
+crime_df = crime_df.withColumn("Street", concat_ws(" ", col("BlockArray")[2], col("BlockArray")[3]))
+
+# Convierte todas las letras a mayúsculas en la columna 'Street'
+crime_df = crime_df.withColumn("Street", upper(col("Street")))
+
+# Filtra las filas donde la columna 'Street' no es vacía
+filtered_df = crime_df.filter(col("Street") != "")
+
+# Agrupa por 'District' y recopila las calles únicas en una lista
+street_list_by_district_df = filtered_df.groupBy("District").agg(
+    concat_ws(", ", collect_set("Street")).alias("StreetList")
+)
+
+# Escribe los resultados en un archivo CSV
+street_list_by_district_df.write.csv("street_list_by_district")
+
+# Detén la sesión de Spark
+spark.stop()
