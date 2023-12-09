fdf21ad52c8bed37ae1a1e8d70442b3a996ad9c8
JorgeSierra22 <jorsie01@ucm.es>
2023-12-09 12:37:10 +0100

    ff

--- code/crimesWithArrest.py
+++ code/crimesWithArrest.py
@@ -1,8 +1,8 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, sum as spark_sum
-
-# Configuración de Spark
+#from pyspark.sql.functions import col, sum as spark_sum
+from pyspark.sql.functions import col, count
+# Configuración de Spark  spark-submit crimesWithArrest.py
 conf = SparkConf().setAppName('ArrestedCrimesSummary')
 sc = SparkContext(conf=conf)
 sc.setLogLevel('ERROR')
@@ -18,7 +18,8 @@ arrested_crimes_df = crime_df.filter(col("Arrest") == "true")
 df = arrested_crimes_df.select("Primary Type", "Year")
 
 # Agrupa por 'Primary Type', cuenta la frecuencia de arrestos por tipo de delito
-arrested_crime_counts_df = df.groupBy("Primary Type").agg(spark_sum("Year").alias("TotalArrestedCrimes"))
+#arrested_crime_counts_df = df.groupBy("Primary Type").agg(spark_sum("Year").alias("TotalArrestedCrimes"))
+arrested_crime_counts_df = arrested_crimes_df.groupBy("Primary Type").agg(count("*").alias("TotalArrestedCrimes"))
 
-# Escribe los resultados en un archivo CSV
+# Escribe los resultados en un archivo CSV  more arrested_crimes_summary/*
 arrested_crime_counts_df.write.csv("arrested_crimes_summary")
